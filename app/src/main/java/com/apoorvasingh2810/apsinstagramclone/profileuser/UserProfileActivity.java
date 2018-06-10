1e7705adeb4aa90e4e18c9ca1af6d0a8d9c01a30
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-06-11 01:14:50 +0530

    User Profile Activity( UI ) - added

---
+++ app/src/main/java/com/apoorvasingh2810/apsinstagramclone/profileuser/UserProfileActivity.java
@@ -0,0 +1,13 @@
+package com.apoorvasingh2810.apsinstagramclone.profileuser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
