c4361a1272406087172f46df9e289a6f8bfd8adf
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-06-11 00:32:01 +0530

    Register User Activity( UI )- added

---
+++ app/src/main/java/com/apoorvasingh2810/apsinstagramclone/RegisterUserActivity.java
@@ -0,0 +1,13 @@
+package com.apoorvasingh2810.apsinstagramclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
